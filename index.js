28349e1279f1dd5e8a628052ff5a9ebd241c2e2e
Jhn <jhonata.tenorio2526@gmail.com>
2021-01-11 14:42:34 -0300

    init twit api

---
+++ index.js
@@ -0,0 +1,15 @@
+var Twit = require('twit')
+
+var T = new Twit({
+  consumer_key:         '...',
+  consumer_secret:      '...',
+  access_token:         '...',
+  access_token_secret:  '...',
+  timeout_ms:           60*1000,  // optional HTTP request timeout to apply to all requests.
+  strictSSL:            true,     // optional - requires SSL certificates to be valid.
+})
+
+//  tweet 'hello world!'
+T.post('statuses/update', { status: 'hello world!' }, function(err, data, response) {
+  console.log(data)
+})
