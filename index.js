599627f23648d8df09d18443812bc844323b82dd
Jhn <jhonata.tenorio2526@gmail.com>
2021-01-14 10:17:57 -0300

    Auth

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
-var Twit = require('twit')
+require('dotenv').config();
+let Twit = require('twit')
 
-var T = new Twit({
-  consumer_key:         '...',
-  consumer_secret:      '...',
-  access_token:         '...',
-  access_token_secret:  '...',
+let T = new Twit({
+  consumer_key:         process.env.CONSUMER_KEY,
+  consumer_secret:      process.env.CONSUMER_SECRET,
+  access_token:         process.env.ACCESS_TOKEN,
+  access_token_secret:  process.env.ACCESS_TOKEN_SECRET,
   timeout_ms:           60*1000,  // optional HTTP request timeout to apply to all requests.
   strictSSL:            true,     // optional - requires SSL certificates to be valid.
 })
